02889085433c30a2ee447fab575a0a7a744c116f
Alicia Myrie <aliciamyrie@gmail.com>
2022-09-01 23:56:12 -0400

    deployed

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,13 +1,17 @@
+import jwt from 'jsonwebtoken';
 import dbConnect from './dbConnect.js';
+import { secretKey } from '../credentials.js';
 
 export async function getTasks(req, res) {
-  // going to later add by user id to this ..
+  const token = req.headers.authorization;
+  const user = jwt.verify(token, secretKey);
   const db = dbConnect();
-  const collection = await db
-    .collection('tasks')
+  const collection = await db.collection('tasks')
+    .where('userId', '==', user.id)
     .get()
-    .catch((err) => res.status(500).send(err));
-  const tasks = collection.docs.map((doc) => {
+      .catch(err => res.status(500).send(err));
+  const tasks = collection.docs.map(doc => {
+    // return {...doc.data(), id: doc.id }
     let task = doc.data();
     task.id = doc.id;
     return task;
@@ -16,29 +20,29 @@ export async function getTasks(req, res) {
 }
 
 export async function createTask(req, res) {
-  const newTask = req.body;
-  if (!newTask || !newTask.task) {
+  const token = req.headers.authorization;
+  let newTask = req.body;
+  const user = jwt.verify(token, secretKey);
+  if (!newTask || !newTask.task || !user) {
     res.status(400).send({ success: false, message: 'Invalid request' });
     return;
   }
+  newTask.userId = user.id;
   const db = dbConnect();
-  await db
-    .collection('tasks')
-    .add(newTask)
-    .catch((err) => res.status(500).send(err));
+  await db.collection('tasks').add(newTask)
+    .catch(err => res.status(500).send(err));
   res.status(201);
-  getTasks(req, res); // send back the full lists of tasks ..
+  getTasks(req, res); // send back the full list of tasks...
 }
 
 export async function updateTask(req, res) {
-  // later we will add user id and timestamp ..
   const taskUpdate = req.body;
   const { taskId } = req.params;
-  const db = dbConnect()
-  await db.collection("tasks").doc(taskId).update(taskUpdate)
-  .catch(err => res.status(500).send(err));
+  const db = dbConnect();
+  await db.collection('tasks').doc(taskId).update(taskUpdate)
+    .catch(err => res.status(500).send(err));
   res.status(202);
-  getTasks(req, res)
+  getTasks(req, res);
 }
 
 export function deleteTask(req, res) {
